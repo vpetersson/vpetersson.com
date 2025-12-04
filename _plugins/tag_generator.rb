88b31587a2ad88f42ef6da570b836b0d87cebefe
Viktor Petersson <self@vpetersson.com>
2025-12-04 19:24:53 +0000

    Hopefully fixes tags

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -1,11 +1,18 @@
-Jekyll::Hooks.register :posts, :post_write do |post|
+Jekyll::Hooks.register :site, :post_read do |site|
+  # Ensure _tags directory exists
+  Dir.mkdir("_tags") unless Dir.exist?("_tags")
+  
   all_existing_tags = Dir.entries("_tags")
                          .map { |t| t.match(/(.*).md/) }
                          .compact
                          .map { |m| m[1].downcase }
 
-  tags = post['tags'].reject { |t| t.empty? }
-  tags.each do |tag|
+  # Collect all tags from all posts
+  all_tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }
+                             .reject { |t| t.nil? || t.empty? }
+                             .uniq
+
+  all_tags.each do |tag|
     hyphenated_tag = tag.gsub(' ', '-').downcase
     generate_tag_file(tag, hyphenated_tag) unless all_existing_tags.include?(hyphenated_tag)
   end
